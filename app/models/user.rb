bf1e73e7490925bc950b0350511fb9dfb148e593
Keon Ye <jnuyeyizhou@gmail.com>
2018-03-14 23:57:57 +0800

    add the verifications for name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
-  # associtations
+
+  # Verifications
+  validates :name, presence: true
+  validates :email, presence: true
+
+  # Associtations
   has_many :microposts
 
 end
