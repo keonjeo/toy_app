a60c1bdca93963fb4806af2f46ecf24b9f54e833
Keon Ye <jnuyeyizhou@gmail.com>
2018-03-14 23:56:21 +0800

    validate the content if exist

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,7 @@
 class Micropost < ApplicationRecord
 
   # Validations
-  validates :content, length: { maximum: 140 }
+  validates :content, length: { maximum: 140 }, presence: true
 
   # Associations
   belong_to :user
